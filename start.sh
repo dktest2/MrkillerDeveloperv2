5e4665347968ffc2ede0a3511c81f44faa9aeae0
Mr Killer 1109 <69214079+harshil8981@users.noreply.github.com>
2023-08-24 19:18:34 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/lazyindu/wowbot.git /LazyPrincess
+  git clone https://github.com/harshil8981/MrkillerDeveloperv2 /MrkillerDeveloperv2
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /LazyPrincess
+  git clone $UPSTREAM_REPO /MrkillerDeveloperv2
 fi
-cd /LazyPrincess
+cd /MrkillerDeveloperv2
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
