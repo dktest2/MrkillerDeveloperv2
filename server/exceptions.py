0b35226deba356a285c43a0b965703fe6f6b57f7
Mr Killer 1109 <69214079+harshil8981@users.noreply.github.com>
2023-08-24 17:58:08 +0530

    Add files via upload

---
+++ server/exceptions.py
@@ -0,0 +1,14 @@
+    # Credit @harshil8981.
+    # Please Don't remove credit.
+    # Born to make history @harshil8981 !
+
+    # Thank you MrkillerDeveloper for helping us in this Journey
+    # 🥰  Thank you for giving me credit @harshil8981  🥰
+    # thanks Adarsh Goel for Server Routing
+    # for any error please contact me -> telegram@Mrkiller_1109 or insta @Mrking_motivational
+
+class InvalidHash(Exception):
+    message = "Invalid hash"
+
+class FIleNotFound(Exception):
+    message = "File not found"
