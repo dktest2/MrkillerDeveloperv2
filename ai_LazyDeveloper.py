d4f6efe91574ecee3745630e65557a48713c1028
Mr Killer 1109 <69214079+harshil8981@users.noreply.github.com>
2023-08-24 17:46:22 +0530

    Add files via upload

---
+++ ai_LazyDeveloper.py
@@ -0,0 +1,41 @@
+#
+#         i am NOT INTRESTED IN BEING YOUR SECOND FATHER... SO DON'T REMOVE MY CREDIT...
+#                                       @harshil8981 
+# 
+# 
+# 
+# 
+#
+#
+#
+#
+#
+#
+#
+#                                                ...PRESENTING...
+#                                  A R T I F i C I A L  -  I N T E L i G E N C E
+#                                              .in  Auto-Filter-Bot.
+#                                                 @harshil8981
+#
+#                                       
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#         this line is for copy-pasters...
+#         ...while you are removing my credit and calling yourself a developerr... 
+#         _____ just imagine, At that time i am fucking your mom and sis at same time, harder & too harder...
+#
+# Credit @harshil8981.
+# Please Don't remove credit.
+# Born to make history @harshil8981 !
+# Thank you LazyDeveloper for helping us in this Journey
+# 🥰  Thank you for giving me credit @harshil8981  🥰
+# for any error please contact me -> telegram@Mrkiller_1109 or insta @Mrking_motivational 
+# rip paid developers 🤣 - >> No need to buy paid source code while @Mrkiller_1109 is here 😍😍
