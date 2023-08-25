7b660681d228de53e3e9e43aadbefbee12fd51e5
dktest2 <120356313+dktest2@users.noreply.github.com>
2023-08-25 12:34:56 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/lazyindu/wowbot.git /LazyPrincess
+  git clone https://github.com/dktest2/MrkillerDeveloperv2.git /LazyPrincess
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /LazyPrincess
